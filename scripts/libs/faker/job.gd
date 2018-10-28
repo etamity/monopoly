1306dd64fe34b0c465ca690c8df1e02b1afe7598
joey.etamity <etamity@joeyetamitys-Mac-Pro.local>
2018-10-28 01:07:21 +0100

    Update

---
+++ scripts/libs/faker/job.gd
@@ -0,0 +1,129 @@
+extends Node
+
+var Math = preload('res://scripts/libs/math.gd');
+
+const _suffix = [
+  "Jr.",
+  "Sr.",
+  "I",
+  "II",
+  "III",
+  "IV",
+  "V",
+  "MD",
+  "DDS",
+  "PhD",
+  "DVM"
+];
+
+const _descriptor = [
+    "Lead",
+    "Senior",
+    "Direct",
+    "Corporate",
+    "Dynamic",
+    "Future",
+    "Product",
+    "National",
+    "Regional",
+    "District",
+    "Central",
+    "Global",
+    "Customer",
+    "Investor",
+    "Dynamic",
+    "International",
+    "Legacy",
+    "Forward",
+    "Internal",
+    "Human",
+    "Chief",
+    "Principal"
+];
+
+const _level = [
+    "Solutions",
+    "Program",
+    "Brand",
+    "Security",
+    "Research",
+    "Marketing",
+    "Directives",
+    "Implementation",
+    "Integration",
+    "Functionality",
+    "Response",
+    "Paradigm",
+    "Tactics",
+    "Identity",
+    "Markets",
+    "Group",
+    "Division",
+    "Applications",
+    "Optimization",
+    "Operations",
+    "Infrastructure",
+    "Intranet",
+    "Communications",
+    "Web",
+    "Branding",
+    "Quality",
+    "Assurance",
+    "Mobility",
+    "Accounts",
+    "Data",
+    "Creative",
+    "Configuration",
+    "Accountability",
+    "Interactions",
+    "Factors",
+    "Usability",
+    "Metrics"
+];
+
+const _job = [
+    "Supervisor",
+    "Associate",
+    "Executive",
+    "Liaison",
+    "Officer",
+    "Manager",
+    "Engineer",
+    "Specialist",
+    "Director",
+    "Coordinator",
+    "Administrator",
+    "Architect",
+    "Analyst",
+    "Designer",
+    "Planner",
+    "Orchestrator",
+    "Technician",
+    "Developer",
+    "Producer",
+    "Consultant",
+    "Assistant",
+    "Facilitator",
+    "Agent",
+    "Representative",
+    "Strategist"
+];
+
+
+func suffix():
+	return Math.random_array(_suffix);
+
+func descriptor():
+	return Math.random_array(_descriptor);
+
+func level():
+	return Math.random_array(_level);
+
+func job():
+	return Math.random_array(_job);
+	
+func job_name():
+	var formats = [
+	'{descriptor} {job}'
+	];
+	
